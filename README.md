6b8ad8d151099d480a0545bd73343749b445a59e
ChangShiRaine <changshi@utexas.edu>
2022-05-09 20:44:05 -0500

    final push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RL_project_DGN
+
+Run DGN or DQN by
+`python3 main_action_preference.py` or `python3 main_epsilon_greedy.py`
+based on the action selection method you choose.
+
+The models are in "model_DGN.py" and "model_DQN.py" 
--- starcraft/main_epsilon_greedy.py
+++ starcraft/main_epsilon_greedy.py
@@ -7,10 +7,7 @@ from config import *
 from utilis import *
 import torch
 import torch.optim as optim
-
-
-env = StarCraft2Env(map_name='25m')
-# env = StarCraft2Env(map_name='25m',range_=range_)
+env = StarCraft2Env(map_name='25m') #,range_=range_)
 env_info = env.get_env_info()
 n_ant = env_info["n_agents"]
 n_actions = env_info["n_actions"]
@@ -19,36 +16,40 @@ obs_space = env_info["obs_shape"] + n_ant
 buff = ReplayBuffer(capacity,obs_space,n_actions,n_ant)
 model = DGN(n_ant,obs_space,hidden_dim,n_actions)
 model_tar = DGN(n_ant,obs_space,hidden_dim,n_actions)
-model = model#.cuda()
-model_tar = model_tar#.cuda()
-model_tar.load_state_dict(model.state_dict())  # todo: what is this doing?
-optimizer = optim.RMSprop(model.parameters(), lr=0.0005)
-# todo: figure out sys.argv
-comm_flag = 1#float(sys.argv[1])
+model = model.cuda()
+model_tar = model_tar.cuda()
+model_tar.load_state_dict(model.state_dict())
+optimizer = optim.RMSprop(model.parameters(), lr = 0.0005)
+# comm_flag = float(sys.argv[1])
+comm_flag = 1
 
 f = open('1'+'_'+'2'+'.txt','w')
+
 # f = open(sys.argv[1]+'_'+sys.argv[2]+'.txt','w')
+mean_reward = []
+
 while i_episode<n_episode:
 	if time_step > max_timestep:
 		break
 	if i_episode > 100:
 		epsilon -= 0.001
 		if epsilon < 0.02:
-			epsilon = 0.02  # todo: what is epsilon
-	print("i_episode", i_episode)
+			epsilon = 0.02
+	if i_episode % 200 == 0:
+		print("i_episode", i_episode)
 	i_episode+=1
 	env.reset()
 	terminated = False
-	obs = get_obs(env.get_obs(),n_ant)  # todo: what does the this encode
-	adj = env.get_visibility_matrix()[:,0:n_ant]*comm_flag + np.eye(n_ant)  # todo: what is this
+	episode_reward = 0
+	obs = get_obs(env.get_obs(),n_ant)
+	adj = env.get_visibility_matrix()[:,0:n_ant]*comm_flag + np.eye(n_ant)
 	mask = np.array([env.get_avail_agent_actions(i) for i in range(n_ant)])
 	while not terminated:
 		test_flag += 1
 		time_step += 1
 
 		action=[]
-		q = model(torch.Tensor(np.array([obs])), torch.Tensor(np.array([adj])))[0]
-		# q = model(torch.Tensor(np.array([obs])).cuda(), torch.Tensor(np.array([adj])).cuda())[0]
+		q = model(torch.Tensor(np.array([obs])).cuda(), torch.Tensor(np.array([adj])).cuda())[0]
 		for i in range(n_ant):
 			if np.random.rand() < epsilon:
 				avail_actions_ind = np.nonzero(mask[i])[0]
@@ -58,12 +59,15 @@ while i_episode<n_episode:
 			action.append(a)
 			
 		reward, terminated, winner = env.step(action)
+		episode_reward += reward
 		next_obs = get_obs(env.get_obs(),n_ant)
 		next_adj = env.get_visibility_matrix()[:,0:n_ant]*comm_flag + np.eye(n_ant)
 		mask = np.array([env.get_avail_agent_actions(i) for i in range(n_ant)])
 		buff.add(np.array(obs),action,reward,np.array(next_obs),adj,next_adj,mask,terminated)
 		obs = next_obs
 		adj = next_adj
+
+	mean_reward.append(episode_reward)
 
 	if test_flag > 10000:
 		log_r, log_w = test_agent(env, model, n_ant)
@@ -72,27 +76,23 @@ while i_episode<n_episode:
 		f.flush()
 		test_flag = 0
 	
-	# if i_episode < 100:
-	# 	continue
+	if i_episode < 100:
+		continue
 
 	for epoch in range(n_epoch):
 		
 		O,A,R,Next_O,Matrix,Next_Matrix,Next_Mask,D = buff.getBatch(batch_size)
 
-		q_values = model(torch.Tensor(O), torch.Tensor(Matrix))
-		# q_values = model(torch.Tensor(O).cuda(), torch.Tensor(Matrix).cuda())
-		target_q_values = model_tar(torch.Tensor(Next_O), torch.Tensor(Next_Matrix))
-		# target_q_values = model_tar(torch.Tensor(Next_O).cuda(), torch.Tensor(Next_Matrix).cuda())
-		target_q_values = (target_q_values - 9e15*(1 - torch.Tensor(Next_Mask))).max(dim = 2)[0]
-		# target_q_values = (target_q_values - 9e15*(1 - torch.Tensor(Next_Mask).cuda())).max(dim = 2)[0]
+		q_values = model(torch.Tensor(O).cuda(), torch.Tensor(Matrix).cuda())
+		target_q_values = model_tar(torch.Tensor(Next_O).cuda(), torch.Tensor(Next_Matrix).cuda())
+		target_q_values = (target_q_values - 9e15*(1 - torch.Tensor(Next_Mask).cuda())).max(dim = 2)[0]
 		target_q_values = np.array(target_q_values.cpu().data)
 		expected_q = np.array(q_values.cpu().data)
 		for j in range(batch_size):
 			for i in range(n_ant):
 				expected_q[j][i][A[j][i]] = R[j] + (1-D[j])*GAMMA*target_q_values[j][i]
 		
-		loss = (q_values - torch.Tensor(expected_q)).pow(2).mean()
-		# loss = (q_values - torch.Tensor(expected_q).cuda()).pow(2).mean()
+		loss = (q_values - torch.Tensor(expected_q).cuda()).pow(2).mean()
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
@@ -102,3 +102,6 @@ while i_episode<n_episode:
 				p_targ.data.mul_(tau)
 				p_targ.data.add_((1 - tau) * p.data)
 
+mean_reward = np.array(mean_reward)
+with open('rewards.npy', 'wb') as f:
+    np.save(f, mean_reward)
